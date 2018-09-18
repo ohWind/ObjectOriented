a57e67ba1929bdaa07d6a2b4f9f195e09e3274de
ohWind <43326510+ohWind@users.noreply.github.com>
2018-09-18 10:24:15 +0800

    创建“人”类练习
    类与对象预习

---
+++ Example1.java
@@ -0,0 +1,15 @@
+package cn.example;
+
+public class Example1 {
+	/**
+	 * 实例练习
+	 * 定义“人”类
+	 */
+	public String name;          //姓名
+	public String gender;        //姓别
+	public int age;                  //年龄
+	public void work(){
+		System.out.println(this.name+"的工作理念：干活挣钱有饭吃");
+	}
+
+}
